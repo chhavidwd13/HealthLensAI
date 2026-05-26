109c33bf48205c49c81517e68603bd7d706a7bbf
Chhavi <chhavi.dwd@gmail.com>
2026-05-27 00:37:59 +0530

    Fixed Streamlit secret API handling

--- modules/gemini_client.py
+++ modules/gemini_client.py
@@ -1,25 +1,35 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 import google.generativeai as genai
 
 load_dotenv()
 
-api_key = os.getenv("GEMINI_API_KEY")
+api_key = st.secrets.get(
+    "GEMINI_API_KEY",
+    os.getenv("GEMINI_API_KEY")
+)
 
 if not api_key:
-    raise ValueError("GEMINI_API_KEY is missing. Please add it inside .env file.")
+    raise ValueError(
+        "GEMINI_API_KEY is missing."
+    )
 
 genai.configure(api_key=api_key)
 
-gemini_model = genai.GenerativeModel("gemini-1.5-flash")
+gemini_model = genai.GenerativeModel(
+    "gemini-1.5-flash"
+)
 
 
 def generate_response(prompt):
+
     try:
         response = gemini_model.generate_content(prompt)
         return response.text
 
     except Exception as error:
+
         return f"""
 Something went wrong while generating AI response.
 
